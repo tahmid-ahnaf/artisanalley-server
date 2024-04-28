738003b8225efa38903bfa85730172876c14f20c
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-04-28 23:16:16 +0600

    get all items and post single item done

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("CRUD Server is running");
+});
 
-app.get('/', (req,res)=>{
-    res.send("CRUD Server is running");
-})
-
-
-app.listen(port, ()=>{
-    console.log(`CRUD Server is running ${port}`);
-})
-
-
-
+app.listen(port, () => {
+  console.log(`CRUD Server is running ${port}`);
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.buwy59t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,16 +25,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const itemCollection = client.db("artcraftDB").collection("item");
+
+    app.get("/items", async (req, res) => {
+      const cursor = itemCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    
+
+    app.post("/items", async (req, res) => {
+      const newItem = req.body;
+      const result = await itemCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
